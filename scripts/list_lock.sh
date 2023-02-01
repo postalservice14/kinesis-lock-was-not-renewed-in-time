afb40adf7ed793d700826947f010bc701e686ca3
John Kelly <john.kelly@ramseysolutions.com>
2023-02-01 14:41:30 -0600

    Upgrade spring-integration-aws to 2.5.4

--- scripts/list_lock.sh
+++ scripts/list_lock.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-aws --endpoint-url=http://localhost:4566 dynamodb scan --table-name SpringIntegrationLockRegistry | jq '.Items[] | {lockKey: .lockKey, ownerName: .ownerName}'
+awslocal --region=eu-west-2 dynamodb scan --table-name SpringIntegrationLockRegistry | jq '.Items[] | {lockKey: .lockKey, ownerName: .ownerName}'
